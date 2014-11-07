a36e0a6d72f8caf8307e281201163e670358a391
estj92 <estj92@gmail.com>
2014-11-07 11:54:15 +0100

    Added solution, added User class.

---
+++ P7-Generate-Use-Data/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P7_Generate_Use_Data
+{
+    class User
+    {
+        public User(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
