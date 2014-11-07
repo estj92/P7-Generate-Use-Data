1db834ffc5b3a8e30e3cc4a0ee93d4574dc1dfb0
estj92 <estj92@gmail.com>
2014-11-07 12:17:02 +0100

    Added coordinate class.

---
+++ P7-Generate-Use-Data/Coordinate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P7_Generate_Use_Data
+{
+    class Coordinate
+    {
+        public Coordinate(double longtitude, double lattitude)
+        {
+            Longtitude = longtitude;
+            Lattitude = lattitude;
+        }
+
+        public double Longtitude { get; set; }
+        public double Lattitude { get; set; }
+    }
+}
