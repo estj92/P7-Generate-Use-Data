4929f120c4f78024232ce080917d5d920473a2e2
estj92 <estj92@gmail.com>
2014-11-10 09:19:31 +0100

    Add Trip class properties

--- P7-Generate-Use-Data/Trip.cs
+++ P7-Generate-Use-Data/Trip.cs
@@ -8,5 +8,21 @@ namespace P7_Generate_Use_Data
 {
     class Trip
     {
+        public Trip(User user, Bike bike, DateTime startTime, DateTime endTime, Coordinate startLocation, Coordinate endLocation)
+        {
+            User = user;
+            Bike = bike;
+            StartTime = startTime;
+            Endtime = endTime;
+            StartLocation=startLocation;
+            EndLocation = endLocation;
+        }
+
+        public User User { get; set; }
+        public Bike Bike { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime Endtime { get; set; }
+        public Coordinate StartLocation { get; set; }
+        public Coordinate EndLocation { get; set; }
     }
 }
